f6c54a0de9a05e34dda3e66d3c926a360aa436ea
Vadym Hrechukha <vadimgrechuha@gmail.com>
2025-02-12 06:34:52 +0200

    HP-2339 refresh last_sale_mat view only if Sale entity passed

--- src/Events/EntitiesSavedEvent.php
+++ src/Events/EntitiesSavedEvent.php
@@ -4,10 +4,15 @@ namespace hiqdev\DataMapper\Events;
 
 class EntitiesSavedEvent
 {
-    public array $entities;
+    private array $entities;
 
     public function __construct(array $entities)
     {
         $this->entities = $entities;
+    }
+
+    public function getEntities(): array
+    {
+        return $this->entities;
     }
 }
